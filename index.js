7e1a946e3edfab554013a966c85d0e4c2d01e29a
Sanyasirao Mopada <sanyasirao.mopada@gmail.com>
2021-06-09 13:24:54 +0000

    Add create book api

--- index.js
+++ index.js
@@ -49,3 +49,40 @@ app.get("/book/:bookId", async (request, response) => {
   const book = await db.get(getBookQuery);
   response.send(book);
 });
+
+app.post("/book/", async (request, response) => {
+  const bookDetails = request.data;
+  const {
+    title,
+    author_id: authorId,
+    rating,
+    rating_count: ratingCount,
+    review_count: reviewCount,
+    description,
+    pages,
+    date_of_publication: dateOfPublication,
+    edition_language: editionLanguage,
+    price,
+    online_stores: onlineStores,
+  } = bookDetails;
+  const createBookQuery = `
+    INSERT INTO
+        book (title, author_id, rating, rating_count, review_count, description, pages, date_of_publication, edition_language, price, online_stores)
+    VALUES (
+        ${title},
+        ${authorId},
+        ${rating},
+        ${ratingCount},
+        ${reviewCount},
+        ${description},
+        ${pages},
+        ${dateOfPublication},
+        ${editionLanguage},
+        ${price},
+        ${onlineStores},
+    );
+    `;
+  const dbResponse = await db.run(createBookQuery);
+  const bookId = dbResponse.lastID;
+  response.send({ book_id: bookId });
+});
